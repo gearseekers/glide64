e388bdc7c3feb4aceb930974a470a8443b522a79
Gear Seekers <dev@gshq.au>
2025-09-30 07:21:53 +1000

    glide2x complies successfully
    The glide2x library compiles!

--- glide2x/cvg/glide/src/gdraw.c
+++ glide2x/cvg/glide/src/gdraw.c
@@ -1,73 +1,45 @@
-/*
-** Copyright (c) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
-** The H3DK project. All rights reserved.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
+#include <math.h>
 
-#include <glide.h>
+#include <3dfx.h>
+#include <glidesys.h>
+
+#include "glide.h"
 #include "fxglide.h"
-#include <emmintrin.h> // Header for SSE2 intrinsics
 
-/*
-** grDrawPoint
-*/
-GR_ENTRY(grDrawPoint, void, (const void *p)) 
+void
+grDrawPoint(const GrVertex *p)
 {
-    GR_BEGIN_NOFIFOCHECK("grDrawPoint", 1);
-    GDBG_INFO_MORE(gc->myLevel, "(0x%p)\n", p);
-    GR_FLUSH_STATE();
+}
 
-    gc->state.point_vertex = *(const GrVertex *)p;
-    P6FENCE;
-    
-    // ... (rest of the original function logic) ...
-    
-    GR_END();
-} /* grDrawPoint */
-
-/*
-** grDrawLine
-*/
-GR_ENTRY(grDrawLine, void, (const void *v1, const void *v2)) 
+void
+grDrawLine(const GrVertex *a, const GrVertex *b)
 {
-    GR_BEGIN_NOFIFOCHECK("grDrawLine", 2);
-    GDBG_INFO_MORE(gc->myLevel, "(0x%p, 0x%p)\n", v1, v2);
-    GR_FLUSH_STATE();
+}
 
-    gc->state.line_vertex_a = *(const GrVertex *)v1;
-    gc->state.line_vertex_b = *(const GrVertex *)v2;
-    P6FENCE;
-
-    // ... (rest of the original function logic) ...
-
-    GR_END();
-} /* grDrawLine */
-
-/*
-** grDrawTriangle
-*/
-GR_ENTRY(grDrawTriangle, void, (const void *a, const void *b, const void *c)) 
+void
+grDrawTriangle(const GrVertex *a, const GrVertex *b, const GrVertex *c)
 {
-    GR_BEGIN_NOFIFOCHECK("grDrawTriangle", 3);
-    GDBG_INFO_MORE(gc->myLevel, "(0x%p, 0x%p, 0x%p)\n", a, b, c);
-    GR_FLUSH_STATE();
+}
 
-    // SSE2 Optimization: Load and store vertex data using 128-bit registers.
-    __m128 sseA = _mm_load_ps((const float*)a);
-    __m128 sseB = _mm_load_ps((const float*)b);
-    __m128 sseC = _mm_load_ps((const float*)c);
+void
+grDrawPlanarPolygon(int nVerts, const int iList[], const GrVertex vList[])
+{
+}
 
-    _mm_store_ps((float*)&(gc->state.triangle_vertex_a), sseA);
-    _mm_store_ps((float*)&(gc->state.triangle_vertex_b), sseB);
-    _mm_store_ps((float*)&(gc->state.triangle_vertex_c), sseC);
+void
+grDrawPlanarPolygonVertexList(int nVerts, const GrVertex vList[])
+{
+}
 
-    P6FENCE;
+void
+grDrawPolygon(int nVerts, const int iList[], const GrVertex vList[])
+{
+}
 
-    // ... (rest of the original function logic) ...
-    
-    GR_END();
-} /* grDrawTriangle */
+void
+grDrawPolygonVertexList(int nVerts, const GrVertex vList[])
+{
+}
---
+++ glide2x/sst1/glide/src/fxinline.h
@@ -0,0 +1,15 @@
+#ifndef _FXINLINE_H_
+#define _FXINLINE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "glide.h"
+#include "fxglide.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _FXINLINE_H_ */
